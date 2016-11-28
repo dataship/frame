fcab5c796156b464f5213f0fb171fc7a4c03c405
Waylon Flinn <waylonflinn@gmail.com>
2016-11-28 10:46:10 -0600

    test for row based construction

--- test/create.js
+++ test/create.js
@@ -15,6 +15,30 @@ test("access column from hidden property", function(t){
 	t.equals(JSON.stringify(frame._cols["a"]), JSON.stringify(a));
 });
 
+test("row based constructor creates columns correctly", function(t){
+	t.plan(2);
+	var rows = [
+		{"a" : 0, "b" : 1},
+		{"a" : 0, "b" : 2},
+		{"a" : 0, "b" : 2},
+		{"a" : 1, "b" : 3},
+		{"a" : 1, "b" : 1},
+		{"a" : 0, "b" : 3},
+		{"a" : 1, "b" : 4},
+		{"a" : 0, "b" : 2},
+		{"a" : 1, "b" : 1},
+	];
+
+	var a = [0, 0, 0, 1, 1, 0, 1, 0, 1];
+	var b = [1, 2, 2, 3, 1, 3, 4, 2, 1];
+
+	var frame = new Frame(rows);
+
+
+	t.equals(JSON.stringify(frame._cols["a"]), JSON.stringify(a));
+	t.equals(JSON.stringify(frame._cols["b"]), JSON.stringify(b));
+});
+
 test("access column as property", function(t){
 	t.plan(1);
 	var a = [0, 0, 0, 1, 1, 0, 1, 0, 1];
