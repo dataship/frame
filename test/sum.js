75c168be3742be0713ae83a0c56ca6e5e056b4a0
Waylon Flinn <waylonflinn@gmail.com>
2017-03-12 09:22:00 -0500

    better column names in test

--- test/sum.js
+++ test/sum.js
@@ -4,14 +4,30 @@ var test = require('tape'),
 test("groupby.sum", function(t){
 	t.plan(1);
 	var frame = new Frame({
-		"group"  : [0, 0, 0, 1, 1, 0, 1, 0, 1],
-		"reduce" : [1, 2, 2, 3, 1, 3, 4, 2, 1]
+		"id"  : [0, 0, 0, 1, 1, 0, 1, 0, 1],
+		"value" : [1, 2, 2, 3, 1, 3, 4, 2, 1]
 	});
 
 	var expected = [10, 9];
 
-	var g = frame.groupby("group");
-	var actual = g.reduce("reduce");
+	var g = frame.groupby("id");
+	var actual = g.sum("value");
+
+	t.equals(JSON.stringify(actual), JSON.stringify(expected), "reduce");
+
+});
+
+test("groupby.sum.strings", function(t){
+	t.plan(1);
+	var frame = new Frame({
+		"id"  : ["a", "a", "a", "b", "b", "a", "b", "a", "b"],
+		"value" : [1, 2, 2, 3, 1, 3, 4, 2, 1]
+	});
+
+	var expected = [10, 9];
+
+	var g = frame.groupby("id");
+	var actual = g.sum("value");
 
 	t.equals(JSON.stringify(actual), JSON.stringify(expected), "reduce");
 
