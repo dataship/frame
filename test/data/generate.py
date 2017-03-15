57831d405ee1203c37387771da134caa8bafbd14
Waylon Flinn <waylonflinn@gmail.com>
2017-03-14 20:54:40 -0500

    enable all numpy numeric types in test data generation

--- test/data/generate.py
+++ test/data/generate.py
@@ -8,10 +8,11 @@ case all data to be recreated.
 
 		"N" - a number of rows to generate
 		"id" - a list of id columns to generate
-			K - list of distinct values
-		 	[{"K" : 3, "strings" : false}, {"K" : 3, "strings" : false}],
+			K - number of distinct values to generate
+			type - type of data to generate for column
+			[{"K" : 3, "type": "int32"}, {"K" : 3, "type": "int32"}],
 		"value" - a list of value columns to generate
-			[{"K" : 100}, {"K" : 100}]
+			[{"K" : 100, "type": "int32"}, {"K" : 100, "type": "int32"}]
 
 Implementing test data generation for a new operation involves two things:
 1. creation of a json spec
@@ -27,11 +28,43 @@ import json
 import numpy as np
 import binary_matrix
 
+OUT_FILENAME = "out.json"
 
-EXTENSION = ".i32"
+extension_map = {
+	"int32" : ".i32",
+	"uint32" : '.u32',
+	"float32" : '.f32',
+	"int64" : '.i64',
+	"uint64" : '.u64',
+	"float64" : '.f64'
+}
 
-def create_column(N, K):
-	return np.random.randint(0, K, N, dtype='int32')
+# function adapted from this issue request on numpy
+# https://github.com/numpy/numpy/issues/3155
+def random_sample(size=None, dtype=np.float64):
+
+	if type(dtype) == str or type(dtype) == unicode:
+		dtype = np.dtype(dtype).type
+
+	type_max = 1 << np.finfo(dtype).nmant
+	sample = np.empty(size, dtype=dtype)
+	sample[...] = np.random.randint(0, type_max, size=size) / dtype(type_max)
+	if size is None:
+		sample = sample[()]
+	return sample
+
+int_types = set(["int32", "int64", "uint64", "uint32"])
+float_types = set(["float32", "float64"])
+
+def create_column(N, K, type="int32"):
+
+	if type in int_types:
+		return np.random.randint(0, K, N, dtype=type)
+
+	if type in float_types:
+		return K * random_sample(N, dtype=type)
+
+	return np.random.randint(0, K, N, dtype="int32")
 
 def write_result(result, location):
 	"""write a dict to a file as a json document"""
@@ -74,39 +107,45 @@ if __name__ == '__main__':
 			os.makedirs(directory)
 
 		# if a result exists, skip this data set
-		if os.path.exists(directory + 'out.json'):
+		if os.path.exists(directory + OUT_FILENAME):
 			print("Skipping {0}".format(directory))
 			continue
 
-		id_names = map(lambda i: "id_{0}".format(i), range(len(options['id'])))
-		value_names = map(lambda i: "value_{0}".format(i), range(len(options['value'])))
+		id_columns = {}
+		for i in range(len(options['id'])):
+			name = "id_{0}".format(i)
+			spec = options['id'][i]
+			dtype = spec['type']
+			if dtype not in extension_map:
+				dtype = "int32"
+			K = spec['K']
 
-		all_exist = False
-		for name in id_names + value_names:
-			all_exist = all_exist & os.path.exists(directory + name + EXTENSION)
+			extension = extension_map[dtype]
+			if os.path.exists(directory + name + extension):
+				column = binary_matrix.read(directory + name + extension)
+			else:
+				column = create_column(N, K, dtype)
+				binary_matrix.write(directory + name + extension, column)
 
-		if all_exist:
-			id_columns = {name : binary_matrix.read(directory + name + EXTENSION) for name in id_names}
-			value_columns = {name : binary_matrix.read(directory + name + EXTENSION) for name in value_names}
+			id_columns[name] = column
 
-		else:
-			id_columns = {}
-			for i in range(len(id_names)):
-				name = id_names[i]
-				spec = options['id'][i]
-				column = create_column(N, spec['K'])
-				id_columns[name] = column
+		value_columns = {}
+		for i in range(len(options['value'])):
+			name = "value_{0}".format(i)
+			spec = options['value'][i]
+			dtype = spec['type']
+			if dtype not in extension_map:
+				dtype = "int32"
+			K = spec['K']
 
-				binary_matrix.write(directory + name + EXTENSION, column)
+			extension = extension_map[dtype]
+			if os.path.exists(directory + name + extension):
+				column = binary_matrix.read(directory + name + extension)
+			else:
+				column = create_column(N, K, dtype)
+				binary_matrix.write(directory + name + extension, column)
 
-			value_columns = {}
-			for i in range(len(value_names)):
-				name = value_names[i]
-				spec = options['value'][i]
-				column = create_column(N, spec['K'])
-				value_columns[name] = column
-
-				binary_matrix.write(directory + name + EXTENSION, column)
+			value_columns[name] = column
 
 		# run reduction
 		arguments = options['arg'] if 'arg' in options else {}
@@ -114,6 +153,6 @@ if __name__ == '__main__':
 
 		# write result
 		#binary_matrix.write(directory + "out.arr", out.flatten())
-		write_result(out, directory + "out.json")
+		write_result(out, directory + OUT_FILENAME)
 
 		print("Created {0}".format(directory))
