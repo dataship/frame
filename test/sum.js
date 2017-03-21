86f024f7c92e7560c55ca9a6b8c93fcdb30e7b50
Waylon Flinn <waylonflinn@gmail.com>
2017-03-21 09:34:40 -0500

    implement tree diff for testing groupby on large and floating point trees

--- test/sum.js
+++ test/sum.js
@@ -62,6 +62,9 @@ tape("groupbymulti.sum", function(t){
 
 */
 
+var RTOL = 1e-05, // 1e-05
+	ATOL = 1e-12; // 1e-12
+
 var dataDirectory = 'test/data/sum/',
 	testFile = 'small.json';
 
@@ -92,6 +95,7 @@ floader.load(dataDirectory + testFile, function(err, config){
 		tape(testName, generateTestCase(directory, names, types, ["value_0"], [test.value[0].type]));
 	}
 });
+
 var OUT_FILENAME = "out.json";
 var DEFAULT_TYPE = "int32";
 
@@ -118,7 +122,14 @@ function generateTestCase(directory, id_names, id_types, value_names, value_type
 				var g = frame.groupbymulti(id_names);
 				var actual = g.summulti(value_names[0]);
 
-				t.equals(JSON.stringify(actual), JSON.stringify(expected));
+				var asrt;
+				if(value_types[0] in float_types){
+					asrt = assert.treeclose;
+				} else {
+					asrt = assert.treeequal;
+				}
+
+				asrt(t, actual, expected, null, RTOL, ATOL);
 			});
 
 		});
@@ -146,6 +157,12 @@ var extension_map = {
 	".f32" : Float32Array,
 	".f64" : Float64Array
 };
+
+var float_types = {
+	"float32" : true,
+	"float64" : true
+}
+
 /* load a binary file as a TypedArray with the type given by the extension */
 function loadArray(filePath, cb){
 
@@ -181,4 +198,159 @@ function load(directory, names, types, callback){
 			callback(err, results);
 		}
 	);
+}
+
+function isobject(obj){ return Object.prototype.toString.call(obj) === "[object Object]";}
+
+/* is there a key in object 'a' not found in object 'b'?
+   if so, return the first key that's not found
+   if not, return null
+ */
+function diffkeys(a, b){
+	same_keys = true;
+	for(key in a){
+		same_keys &= (key in b);
+		if(!same_keys){
+			return key;
+		}
+	}
+
+	return null;
+}
+
+/* comp is s comparison function for leaves
+a - actual
+b - expected
+*/
+function treediff(a, b, comp){
+
+	var p = "<r>";
+	var todo = [[a, b, p]]; // tuple of (a, b, p)
+	var parents = { p : null };
+
+	var diff_key = null,
+		diff_a = null,
+		diff_b = null;
+	var t;
+	while (todo.length > 0 && !diff_key){
+		t = todo.pop();
+		n_a = t[0];
+		n_b = t[1];
+		p = t[2];
+
+		// are all the keys the same?
+		diff_a = diffkeys(n_a, n_b);
+		if(diff_a){
+			diff_key = p;
+			break;
+		}
+		diff_b = diffkeys(n_b, n_a);
+		if(diff_b){
+			diff_key = p;
+			break;
+		}
+
+		// check children
+		for(key in n_b){
+			// both objects/internal nodes?
+			if(isobject(n_b[key]) && isobject(n_a[key])){
+				// yes, add to stack
+				parents[key] = p;
+				todo.push([n_a[key], n_b[key], key]);
+
+			// both leaves?
+			} else if(!isobject(n_b[key]) && !isobject(n_a[key])) {
+				// yes, compare values
+				if(!comp(n_b[key], n_a[key])){
+					diff_key = key;
+					diff_a = n_a[key];
+					diff_b = n_b[key];
+					break;
+				}
+			} else {
+				// one is leaf the other is internal
+				diff_key = key;
+				if(is_object(n_b)){
+					diff_a = n_a[key];
+				} else {
+					diff_b = n_b[key];
+				}
+				break;
+			}
+		}
+	}
+
+	var path;
+	// difference found?
+	if(diff_key){
+		// yes, reconstruct the path
+		var n = diff_key;
+		path = [n];
+		while(parents[n]){
+			n = parents[n];
+			path.push(n);
+		}
+
+		// diff_a and diff_b are both present on a leaf difference
+		// only one is present for an internal node difference
+		return {"path" : path.reverse(), "a" : diff_a, "b" : diff_b};
+	}
+
+	return null;
+
+}
+
+var assert = {};
+
+/* determine whether two trees are equivalent
+*/
+assert.treeequal = function(t, a, b, msg) {
+	var fail = treediff(a, b, function(a_n, b_n){
+		return a_n === b_n;
+	});
+
+	msg = msg || 'should be treeequal';
+	return assert.treeassert(t, fail, msg);
+};
+
+/* determine whether two trees are approximately equivalent:
+internal nodes are identical
+leaves are within specified floating point tolerances
+ */
+assert.treeclose = function(t, a, b, msg, RTOL, ATOL) {
+	RTOL= RTOL || 1e-05;  // for 32 bit precision: 1e-06
+	ATOL= ATOL || 1e-08;
+
+	// treeequal with a floating point comparison function
+	var fail = treediff(a, b, function(a_n, b_n){
+		return Math.abs(a_n - b_n) <= ATOL + RTOL * Math.abs(b_n)
+	});
+
+	msg = msg || 'should be treeclose';
+	return assert.treeassert(t, fail, msg);
+};
+
+assert.treeassert = function(t, fail, msg){
+
+	if(fail){
+		var actual = fail.path.join(" -> "),
+			expected = fail.path.join(" -> ");
+
+		if(fail.a){
+			actual += " -> " + fail.a;
+		}
+		if(fail.b){
+			expected += " -> " + fail.b;
+		}
+	}
+
+	t._assert(!fail, {
+		message : msg,
+		operator : 'treeclose',
+		actual : actual,
+		expected : expected,
+		extra : null
+	});
+
+	return !fail;
 };
